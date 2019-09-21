9412f4b687ed0390791f264e1d961f8886f32989
edwwn <nyakundii@students.kabianga.ac.ke>
2019-09-21 13:08:21 +0300

    Fixed Two bugs in Calculating Balances
    Easier way of calculating balances using Reduce in Functools

--- blockchain.py
+++ blockchain.py
@@ -1,7 +1,10 @@
-# Initializing our blockchain List
+import functools
 
+
+# The reward we give to miners for creating a new block
 MINING_REWARD = 10
 
+# Initializing our blockchain List
 genenis_block = {
     'previous_hash': '',
     'index': 0,
@@ -25,17 +28,17 @@ def get_balance(participant):
                       for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
 
-    amount_sent = 0
-    for tx in tx_sender:
-        if len(tx) > 0:
-            amount_sent += tx[0]
+    print(tx_sender)
+
+    amount_sent = functools.reduce(
+        lambda tx_sum, tx_amt: tx_sum + sum(tx_amt)if len(tx_amt) > 0 else tx_sum + 0, tx_sender, 0)
 
     tx_recipient = [[tx['amount'] for tx in block['transactions']
                      if tx['recipient'] == participant] for block in blockchain]
-    amount_received = 0
-    for tx in tx_recipient:
-        if len(tx) > 0:
-            amount_received += tx[0]
+
+    amount_received = functools.reduce(
+        lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0, tx_recipient, 0)
+
     return amount_received - amount_sent
 
 
@@ -141,9 +144,9 @@ while waiting_for_input:
         tx_data = get_transaction_value()
         recipient, amount = tx_data
         if add_transaction(recipient, amount=amount):
-            print('Added Transaction')
+            print('Added Transaction !!')
         else:
-            print('Transaction Failed')
+            print('Transaction Failed !!')
 
         print(open_transactions)
     elif user_choice == '2':
@@ -175,7 +178,7 @@ while waiting_for_input:
         print_blockchain_elements()
         print('Invalid Blockchain')
         break
-    print(get_balance('Edu'))
+    print('Balance of {}: {:6.2f}'.format('Edu', get_balance('Edu')))
 else:
     print('User Left')
 
---
+++ functions.py
@@ -0,0 +1,7 @@
+def ulimited_arguments(*args):
+    print(args)
+    for argument in args:
+        print(argument)
+
+
+ulimited_arguments(1, 2, 3, 4)
